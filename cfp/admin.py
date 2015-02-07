29f3fd3e9b303992916ba9b7591f5ba667368e26
Kyle Conroy <kyle@kyleconroy.com>
2015-02-07 03:36:08 -0800

    Admin is somewhat working

--- cfp/admin.py
+++ cfp/admin.py
@@ -4,11 +4,37 @@ from cfp.models import Call
 from cfp.models import Conference
 
 
+class CallInline(admin.StackedInline):
+    model = Call
+    extra = 0
+    readonly_fields = ('state',)
+
+
+def make_approved(modeladmin, request, queryset):
+    for conf in queryset.all():
+        conf.call.approve()
+        conf.call.save()
+make_approved.short_description = "Mark selected calls as approved"
+
+
+def make_rejected(modeladmin, request, queryset):
+    for conf in queryset.all():
+        conf.call.reject()
+        conf.call.save()
+make_rejected.short_description = "Mark selected calls as rejected"
+
+
 @admin.register(Conference)
 class ConferenceAdmin(admin.ModelAdmin):
+    inlines = [
+        CallInline,
+    ]
+    list_filter = ('call__state',)
     list_display = ('name', 'start', 'end')
+    actions = [make_approved, make_rejected]
 
 
 @admin.register(Call)
 class CallAdmin(admin.ModelAdmin):
-    list_display = ('conference', 'start', 'end')
+    list_display = ('conference', 'start', 'end', 'state')
+    readonly_fields = ('state',)
