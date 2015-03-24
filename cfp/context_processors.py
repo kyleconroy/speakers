bb5396c0dcc77efd6d55b2741271fc413acc0b14
Kyle Conroy <kyle@kyleconroy.com>
2015-03-24 01:33:22 -0700

    Add brand new style

--- cfp/context_processors.py
+++ cfp/context_processors.py
@@ -1,7 +1,8 @@
 from cfp.models import Profile
 
+
 def empty_profile(request):
-    if request.user.is_authenticated(): 
+    if request.user.is_authenticated():
         profile = Profile.generate(request.user)
         return {'empty_profile': profile.is_empty()}
     return {}
---
+++ cfp/templatetags/gravatar.py
@@ -0,0 +1,17 @@
+import hashlib
+import urllib.parse
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def gravatarize(value):
+    h = hashlib.md5(value.lower().encode('utf-8')).hexdigest()
+    gravatar_url = "http://www.gravatar.com/avatar/" + h + "?"
+    return gravatar_url + urllib.parse.urlencode({
+        'd': 'avatar@calltospeakers.com',
+        's': 50,
+    })
