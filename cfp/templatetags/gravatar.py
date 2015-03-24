55a147b65255ad5bc18003870ce6e910ce31606f
Kyle Conroy <kyle@kyleconroy.com>
2015-03-24 01:39:04 -0700

    Use SSL for gravatar

--- cfp/templatetags/gravatar.py
+++ cfp/templatetags/gravatar.py
@@ -10,7 +10,7 @@ register = template.Library()
 @register.filter
 def gravatarize(value):
     h = hashlib.md5(value.lower().encode('utf-8')).hexdigest()
-    gravatar_url = "http://www.gravatar.com/avatar/" + h + "?"
+    gravatar_url = "https://www.gravatar.com/avatar/" + h + "?"
     return gravatar_url + urllib.parse.urlencode({
         'd': 'avatar@calltospeakers.com',
         's': 50,
